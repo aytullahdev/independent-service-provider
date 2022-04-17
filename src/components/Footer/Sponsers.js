b9d7313a7528847fbfcb33898fb594d2b95a57dd
unknown <ayt.ullah.dev@gmail.com>
2022-04-18 03:40:00 +0600

    sponser section added

---
+++ src/components/Footer/Sponsers.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Sponsers = () => {
+    return (
+        <div className='w-full grid grid-cols-4 lg:grid-cols-8 gap-4 my-10'>
+            <div>
+                <img  className='w-20  mx-auto  h-20' src="http://zipsprout.com/wp-content/uploads/2017/08/mcdonalds.png" alt="" />
+            </div>
+            <div>
+                <img  className='w-20 mx-auto  h-20' src="http://zipsprout.com/wp-content/uploads/2017/08/ibm.png" alt="" />
+            </div>
+            <div>
+                <img className='w-20 mx-auto  h-20'  src="http://zipsprout.com/wp-content/uploads/2017/08/barefoot.png" alt="" />
+            </div>
+            <div>
+                <img  className='w-20  mx-auto h-20' src="http://zipsprout.com/wp-content/uploads/2017/08/costco.png" alt="" />
+            </div>
+            <div>
+                <img  className='w-20  mx-auto h-20' src="http://zipsprout.com/wp-content/uploads/2017/08/fredmeyer.png" alt="" />
+            </div>
+            <div>
+                <img  className='w-20  mx-auto h-20' src="http://zipsprout.com/wp-content/uploads/2017/08/easternbank.png" alt="" />
+            </div>
+            <div>
+                <img  className='w-20  mx-auto h-20' src="http://zipsprout.com/wp-content/uploads/2017/08/lyft.png" alt="" />
+            </div>
+            <div>
+                <img  className='w-20  mx-auto h-20' src="http://zipsprout.com/wp-content/uploads/2017/08/united.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Sponsers;

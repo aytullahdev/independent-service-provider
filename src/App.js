00b264f6fd2378a8b25effd6547c659d94c3a9f4
unknown <ayt.ullah.dev@gmail.com>
2022-04-17 10:58:09 +0600

    Blogs and About route added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Mainsection from './components/Main/Mainsection';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Mainsection from "./components/Main/Mainsection";
+import Navbar from "./components/Header/Navbar/Navbar";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./components/blogs/Blogs";
+import About from "./components/About/About";
 function App() {
-    const [viewmode, setViewmode] = useState(false)
-    const toggleviewmode=()=>{
-           
-            setViewmode(!viewmode);
-         
-    }
+  const [viewmode, setViewmode] = useState(false);
+  const toggleviewmode = () => {
+    setViewmode(!viewmode);
+  };
   return (
-    <div className={`App  ${viewmode?'dark':' '}`}>
-        <div className='dark:bg-slate-900 bg-gray-100'>
-          <Header toggleviewmode={toggleviewmode}/>
-          <Mainsection/>
-          <Footer/>
-        </div>
+    <div className={`App  ${viewmode ? "dark" : " "}`}>
+      <div className="dark:bg-slate-900 bg-gray-100">
+        <Navbar toggleviewmode={toggleviewmode} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header toggleviewmode={toggleviewmode} />
+                <Mainsection />
+              </>
+            }
+          />
+          <Route path="/services" element={<Mainsection />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
 
-          
-
-
-     </div>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Banner from './Banner/Banner';
-import Navbar from './Navbar/Navbar';
 
 const Header = ({toggleviewmode}) => {
     return (
         <div>
-            <Navbar toggleviewmode={toggleviewmode}/>
+            
             <Banner/>
         </div>
     );
